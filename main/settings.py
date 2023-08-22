2d10425f442150e89fb9633fa050c7c381512394
Sanjay Mulchandani <51833884+sanjaymulchandani@users.noreply.github.com>
2023-08-22 17:56:43 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r#p0#ek-4h7p5y%q7qzda*u_m6^iw9f2l@qt(3o1eq8^$(@j=^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://13.51.194.73:8000/']
+ALLOWED_HOSTS = ['13.51.194.73']
 
 
 # Application definition
